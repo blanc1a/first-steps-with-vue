7e43dafb395489f8856d89cff62d7dbe40f73416
Blanca Ziems <blanca481@hotmail.de>
2023-12-05 14:22:50 +0100

    add 08

---
+++ 08-event-handling/script.js
@@ -0,0 +1,23 @@
+Vue.createApp({
+  data() {
+    return {
+      x: 12,
+      y: 4,
+      fruitBasket: [
+        "🍏 Apple",
+        "🍌 Banana",
+        "🍉 Melon",
+        "🫐 Blueberry",
+        "🍓 Strawberry",
+        "🍍 Ananas",
+        "🥭 Mango",
+      ],
+    };
+  },
+  methods: {
+    removeFruit(index) {
+      this.fruitBasket.splice(index, 1);
+      //um item an dem spezifischen index aus dem array zu entfernen
+    },
+  },
+}).mount("#app");
